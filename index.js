99edd5b7e5a2ce6021c4b1b40351bf4f6c119d1d
senemogluc <senemogluc@mef.edu.tr>
2023-05-17 20:58:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//search button click
+
+$('.topbtn').on('click', function(){
+  
+    $('#topSearch').toggle();
+    
+});
+
+//browse click
+
+$('.red').on('click', function(){
+  $('.browserHide').animate({height: 'toggle'});
+})
+
+//slideshow//
