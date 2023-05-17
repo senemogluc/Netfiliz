7c30cd95796d3ac3aa7447fcd7d9dc6951956d37
senemogluc <senemogluc@mef.edu.tr>
2023-05-17 21:18:31 +0300

    changes

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel = "stylesheet" href="css/style.scss">
+    <title> Netfliz </title>
+</head>  
+<body>
+  <nav>
+    <div class="container">
+      <div class="nav-wrap">
+        <object id="logo" data="http://www.irishexaminer.com/remote/media.central.ie/media/images/n/netflixLogoBlack2015_large.jpg?width=648&s=ie-808746"></object>
+        <div class="menu-left">
+          <ul>
+            <li>
+              <span class="red"> Browse
+              </span><i class="fa fa-caret-down"></i>
+
+              <!--browse hidden navigation open-->
+              <div class="browserHide">
+                <ul>
+                  <li> Childrens</li>
+                  <li> Drama</li>
+                  <li> Horror</li>
+                </ul>
+                <ul>
+                  <li> Anime</li>
+                  <li> Action</li>
+                  <li> Adventure</li>
+                </ul>
+                <ul>
+                  <li> Comedy</li>
+                  <li> Foreign</li>
+                  <li>Netflix Originals</li>
+                </ul>
+
+              </div>
+
+            </li>
+            <li>Kids</li>
+            <li> DVDs</li>
+          </ul>
+        </div>
+        <div class="menu-right">
+          <button class="topbtn"> Search</button>
+          <input type="search" id="topSearch">
+          <i class="fa fa-bell"></i>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid main">
+    <div class="topwrap">
+      <div class="row">
+        <div class="col-sm-4">
+          <h1>21</h1>
+          <h3> New Movies and Shows Added</h3>
+          <h6> SEE ALL RECENTLY ADDED <i class="fa fa-arrow-right"></i></h6>
+          <img class="sideposter" src="http://static.tumblr.com/otaopql/JCPn5iplo/lilylogo.png">
+          <span><button><i class="fa fa-play"></i>Play</button></span>
+          <span><button><i class="fa fa-plus" aria-hidden="true"></i>My List</button></span>
+        </div>
+        <div class="col-sm-8">
+          <img class="mainposter" src="http://www.comingsoon.net/assets/uploads/2017/09/punisher-poster-header3.jpg" </div>
+
+        </div>
+      </div>
+    </div>
+
+    <!--Movie List Row 1-->
+    <div class="container-fluid contentsec">
+      <h2> Trending Now</h2>
+      <div class="row">
+        <div class="col-sm-2">
+          <img class="slide" src="https://cdn3.digitalartsonline.co.uk/cmsdata/slideshow/3662115/baby-driver-rory-hi-res.jpg"><i id="left" class="fa fa-chevron-circle-left" aria-hidden="true"></i>
+
+        </div>
+        <div class="col-sm-2">
+          <img class="slide" src="https://images-na.ssl-images-amazon.com/images/I/71rNJQ2g-EL._SY550_.jpg">
+        </div>
+        <div class="col-sm-2">
+          <img class="slide" src="https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg">
+        </div>
+        <div class="col-sm-2">
+          <img class="slide" src="https://i.pinimg.com/736x/34/b6/df/34b6dfad29b4da1db83ff684bf5ceec8--alternative-movie-posters-star-wars-art.jpg">
+        </div>
+        <div class="col-sm-2">
+          <img class="slide" src="http://gdj.graphicdesignjunction.com/wp-content/uploads/2011/12/grey-movie-poster.jpg">
+        </div>
+        <div class="col-sm-2">
+          <img class="slide" src="http://assets1.ignimgs.com/2015/12/09/pxbww85kdae1gb3rrmbljpg-c73305_1280w.jpg"><i id="right" class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+
+        </div>
+
+        <!--Movie list row 2-->
+        <div class="container-fluid contentsec">
+          <h2> New to Netflix</h2>
+          <div class="row">
+            <div class="col-sm-2">
+              <img class="slide" src="https://foreignpolicymag.files.wordpress.com/2017/05/fury_2014_poster2.jpg"><i id="left" class="fa fa-chevron-circle-left" aria-hidden="true"></i>
+            </div>
+            <div class="col-sm-2">
+              <img class="slide" src="http://img.moviepostershop.com/ghostbusters-movie-poster-2016-1020775586.jpg">
+            </div>
+            <div class="col-sm-2">
+              <img class="slide" src="http://digitalspyuk.cdnds.net/17/12/480x681/john-wick-chapter-2-poster.jpg">
+            </div>
+            <div class="col-sm-2">
+              <img class="slide" src="https://i.pinimg.com/736x/63/bc/fc/63bcfcacfbd1c291055dfa73253a29c7--iconic-movie-posters-horror-movie-posters.jpg">
+            </div>
+            <div class="col-sm-2">
+              <img class="slide" src="http://imgc.allpostersimages.com/img/posters/pulp-fiction-cover-with-uma-thurman-movie-poster_u-L-F13MQF0.jpg">
+            </div>
+            <div class="col-sm-2">
+              <img class="slide" src="http://photos1.blogger.com/x/blogger/4718/1605/1600/392061/poster%20nacho.jpg"><i id="right" class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+            </div>
+</body>
